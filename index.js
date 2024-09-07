40917c29f2ab960f2be1c6c952f5eccd7a2c0a37
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-09-08 09:47:16 +1200

    Got the CLI working in an interactive mode.

--- index.js
+++ index.js
@@ -1,36 +1,77 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-import { program } from "commander";
-import inqurier from "inquirer";
+import inquirer from "inquirer";
+
+const options = [
+  {
+    type: "list",
+    name: "command",
+    message: "Choose an option:",
+    choices: ["Add data", "Print bullshit", "Exit"],
+  },
+];
 
 const questions = [
   {
     type: "input",
-    name: "name", // "name" is correct here
-    message: "Customer name",
+    name: "title",
+    message: "Enter the title of the listing item: ",
   },
   {
     type: "input",
-    name: "age", // "name" should be 'age' to store the answer for age
-    message: "Customer age",
+    name: "description",
+    message: "Enter the description of the listing item: ",
+  },
+  {
+    type: "input",
+    name: "start_price",
+    message: "Enter the start price of the listing: ",
+    validate: function (value) {
+      if (isNaN(value) || parseInt(value) < 1 || value === "") {
+        return "Please enter a valid number.";
+      }
+      return true;
+    },
+    filter: function (value) {
+      return parseFloat(value); // Convert input to float
+    },
+  },
+  {
+    type: "input",
+    name: "reserve_price",
+    message: "Enter the reserve price of the listing: ",
+    validate: function (value) {
+      if (isNaN(value) || value < 0 || value === "") {
+        return "Please enter a valid number.";
+      }
+      return true;
+    },
+    filter: function (value) {
+      return parseFloat(value); // Convert input to float
+    },
   },
 ];
 
-program
-  .command("text")
-  .alias("t")
-  .description("This is a test command")
-  .action(() => {
-    console.log("Hello from test command");
-  });
+const runCLI = async () => {
+  console.clear();
+  console.log("Welcome to the CLI!");
+  while (true) {
+    const { command } = await inquirer.prompt(options);
 
-program
-  .command("add")
-  .alias("a")
-  .description("Add a customer")
-  .action(() => {
-    inqurier.prompt(questions).then((answers) => console.log(answers));
-  });
+    if (command === "Exit") {
+      console.log("Goodbye!");
+      break;
+    }
 
+    if (command === "Add data") {
+      const answers = await inquirer.prompt(questions);
+      console.log("\n\n\n\n\n");
+      console.log(answers);
+      console.log("\n\n\n\n\n");
+    } else if (command === "Print bullshit") {
+      console.log("This is some bullshit...");
+    }
+  }
+};
 
-program.parse(process.argv);
+runCLI();
